13f0f4ece2151f488f90b0c4e44d2dbe747272f5
a-johanson <a-johanson@users.noreply.github.com>
2023-08-18 09:43:37 +0200

    add skeleton for LightDirectionDistanceCanvas

--- src/canvas.rs
+++ src/canvas.rs
@@ -27,6 +27,64 @@ pub trait Canvas {
         )
      }
 }
+pub struct LightDirectionDistanceCanvas {
+    data: Vec<[f32; 3]>,
+    width: u32,
+    height: u32,
+}
+
+impl Canvas for LightDirectionDistanceCanvas {
+    fn width(&self) -> u32 {
+        self.width
+    }
+
+    fn height(&self) -> u32 {
+        self.height
+    }
+}
+
+impl LightDirectionDistanceCanvas {
+    pub fn new(width: u32, height: u32) -> LightDirectionDistanceCanvas {
+        let data_length = (width as usize) * (height as usize);
+        let data = vec![[f32::NAN; 3]; data_length];
+        LightDirectionDistanceCanvas {
+            data,
+            width,
+            height,
+        }
+    }
+
+    fn pixel_index(&self, x: u32, y: u32) -> usize {
+        (self.width as usize) * (y as usize) + (x as usize)
+    }
+
+    pub fn fill(&mut self, lightness: f32, direction: f32, distance: f32) {
+        for v in self.data.iter_mut() {
+            *v = [lightness, direction, distance];
+        }
+    }
+
+    pub fn set_pixel(&mut self, x: u32, y: u32, lightness: f32, direction: f32, distance: f32) {
+        let idx = self.pixel_index(x, y);
+        let v = self.data.get_mut(idx).unwrap();
+        *v = [lightness, direction, distance];
+    }
+
+    pub fn pixel_value(&self, x: u32, y: u32) -> (f32, f32, f32) {
+        let idx = self.pixel_index(x, y);
+        let v = self.data.get(idx).unwrap();
+        (v[0], v[1], v[2])
+    }
+
+    pub fn sample_pixel_value(&self, x: f32, y: f32) -> (f32, f32, f32) {
+        // find up to four relevant pixels, take the weighted average of their values ignoring NANs
+        (0.0, 0.0, 0.0)
+    }
+
+    pub fn to_rgba(&self) -> Vec<u8> {
+        vec![]
+    }
+}
 
 pub struct SkiaCanvas {
     pixmap: Pixmap,
